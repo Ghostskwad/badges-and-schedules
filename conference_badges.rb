0e4d5cd667aedd9106204bf6524653b24c4ca65a
Quade <quadehouston@gmail.com>
2020-12-14 14:36:15 -0500

    commit tree cf302b1e1fd65a0e6e16d48b4b62d5cf6485637e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_array = []
+    
+  names.each do |name| 
+  badge_array << "Hello, my name is #{name}." 
+  end
+  return badge_array
+end
+
+def assign_rooms(speakers)
+  room_assignment = []
+  
+  speakers.each_with_index do |speaker, room|
+    room_assignment << "Hello, #{speaker}! You'll be assigned to room #{room+1}!"
+  end
+  room_assignment
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each {|badge| puts badge}
+    assign_rooms(attendees).each {|rooms| puts rooms}
+end
